2d04297b1e7b23fa347553c7b5202fc7bb2d3c2e
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2011-12-12 15:34:34 +0200

    Transmission trailor YAML info

--- lib/absa-h2h.rb
+++ lib/absa-h2h.rb
@@ -35,24 +35,25 @@ module Absa
       end
       
       class Trailer
+        LAYOUT_RULES = YAML.load(File.open("./lib/config/file_layout_rules.yml"))["transmission"]["trailer"]
         
+        attr_accessor :tt_rec_id, :th_rec_status, :tt_no_of_recs
+        
+        def initialize(options = {})
+          @string = " "*200
+          validate! options
+        end
+        
+        def validate!(options)
+          options.each do |k,v|
+            LAYOUT_RULES[k]
+          end
+        end
+        
+        def to_s
+          
+        end
       end
     end
   end
 end
-
-
-
-# 
-# json_data = {
-#   transmission_header: {
-#     th_rec_id: "000"
-#     th_rec_status: "T"
-#     th_date: 
-#   }
-# }
-# 
-# x = Absa::H2h.build(json_data)
-# x.send
-
-
