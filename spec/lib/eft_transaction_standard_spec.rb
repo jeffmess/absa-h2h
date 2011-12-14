2e54031233a44c3f50678e7e13a05b2383bd995a
Jeffrey van Aswegen <jeffmess@gmail.com>
2011-12-14 16:40:27 +0200

    Adding spec for contra record

--- spec/lib/eft_transaction_standard_spec.rb
+++ spec/lib/eft_transaction_standard_spec.rb
@@ -4,7 +4,7 @@ describe Absa::H2h::Transmission::Eft::StandardRecord do
   before(:each) do
     @hash = {
       transmission: {
-        sections: [{
+        user_sets: [{
           type: 'eft',
           content: {
             transactions: [{
@@ -28,22 +28,53 @@ describe Absa::H2h::Transmission::Eft::StandardRecord do
                 homing_account_name: "HENNIE DU TOIT   040524",
                 homing_institution: "21"
               }
+            },{
+              type: "contra_record",
+              content: {
+                rec_id: "001",
+                rec_status: "T",
+                bankserv_record_identifier: "52",
+                user_branch: "632005",
+                user_nominated_account: "4053538939",
+                user_code: "9534",
+                user_sequence_number: "8",
+                homing_branch: "632005",
+                homing_account_number: "4053538939",
+                type_of_account: "1",
+                amount: "16028000",
+                action_date: Time.now.strftime("%y%m%d"),
+                entry_class: "10",
+                user_reference: "ALIMITTST1CONTRA 040524 08"
+              }
             }],
           }
         }]
       }
     }
-    @eft_transaction = @hash[:transmission][:sections].first[:content][:transactions].first[:content]
+    @eft_transaction = @hash[:transmission][:user_sets].first[:content][:transactions].first[:content]
+    @contra_transaction = @hash[:transmission][:user_sets].first[:content][:transactions].last[:content]
   end
   
   it "should be able to build an eft transaction record" do
-    trailer = Absa::H2h::Transmission::Eft::StandardRecord.new(@eft_transaction)
+    record = Absa::H2h::Transmission::Eft::StandardRecord.new(@eft_transaction)
     today = Time.now.strftime("%y%m%d")
     string =" "*200
     string[0,172] = "001T5063200504053538939953400000163200501019611899100000001000#{today}440   ALIMITTST1SPP    040524 01    HENNIE DU TOIT   040524                                           21"
     string[134, 20] = "0" * 20
     
-    trailer.to_s.should == string
+    record.to_s.should == string
+  end
+  
+  it "should be able to build an eft contra record" do
+    record = Absa::H2h::Transmission::Eft::ContraRecord.new(@contra_transaction)
+    
+    today = Time.now.strftime("%y%m%d")
+    string =" "*200
+    string[0,104] = "001T5263200504053538939953400000863200504053538939100016028000#{today}100000ALIMITTST1CONTRA 040524 08    "
+    
+    puts record.to_s.length
+    puts string.length
+    record.to_s.should == string
   end
 
   
