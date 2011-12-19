c4058edf23af258bf1b307711e33e46db01a11f8
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2011-12-19 12:48:55 +0200

    Add folder

---
+++ spec/lib/transmission/trailer_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Absa::H2h do
+  
+  before(:each) do
+    @hash = {
+      transmission: {
+        trailer: {
+          rec_id: "999",
+          tt_rec_status: "T",
+          tt_no_of_recs: "7",
+        }
+      }
+    }
+  end
+  
+  it "should be able to build a document trailer" do
+    trailer = Absa::H2h::Transmission::Trailer.new(@hash[:transmission][:trailer])
+
+    string = " " * 200
+    string[0,13] = "999T000000007"
+
+    trailer.to_s.should == string
+  end
+  
+end
