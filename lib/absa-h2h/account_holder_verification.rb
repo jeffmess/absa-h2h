311d8f1af566a3a6771e2cd29fcb721ea0c1e697
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2011-12-19 12:34:12 +0200

    Minor cleanup

--- lib/absa-h2h/account_holder_verification.rb
+++ lib/absa-h2h/account_holder_verification.rb
@@ -8,18 +8,6 @@ module Absa
         class InternalAccountDetail < Record; end
         class ExternalAccountDetail < Record; end
         
-        # def self.build(content)
-        #           transactions = content[:transactions].map do |transaction|
-        #             class_name = "Absa::H2h::Transmission::AccountHolderVerification::#{transaction[:type].camelize}"
-        #             class_name.constantize.new(transaction[:content])
-        #           end
-        #           
-        #           {
-        #             header: Header.new(content[:header]),
-        #             trailer: Trailer.new(content[:trailer]),
-        #             transactions: transactions            
-        #           }          
-        #         end
       end
       
     end
--- lib/absa-h2h/transmission/trailer.rb
+++ lib/absa-h2h/transmission/trailer.rb
@@ -1,7 +1,6 @@
 module Absa
   module H2h
     module Transmission
-      class Header < Record; end
       class Trailer < Record; end
     end
   end
