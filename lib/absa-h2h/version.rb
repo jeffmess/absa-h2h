5b4bc5a98efb2c421ec2a033166ad4af4beaaced
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2012-01-18 11:59:58 +0200

    Change reason codes to strings

--- lib/absa-h2h/version.rb
+++ lib/absa-h2h/version.rb
@@ -1,5 +1,5 @@
 module Absa
   module H2h
-    VERSION = "0.0.6"
+    VERSION = "0.0.7"
   end
 end
--- spec/lib/eft/rejection_code_spec.rb
+++ spec/lib/eft/rejection_code_spec.rb
@@ -8,13 +8,13 @@ describe Absa::H2h::Eft::RejectionCode do
     list.length.should == 52
 
     list.each do |code, reason|
-      code.is_a?(Fixnum).should be_true
+      code.is_a?(String).should be_true
       reason.is_a?(String).should be_true
     end
   end
   
   it "should retrieve a rejection reason for a given code" do
-    reason = Absa::H2h::Eft::RejectionCode.reason_for_code(2)
+    reason = Absa::H2h::Eft::RejectionCode.reason_for_code("2")
     reason.should == "NOT PROVIDED FOR"
   end
   
@@ -24,13 +24,13 @@ describe Absa::H2h::Eft::RejectionCode do
     list.length.should == 267
     
     list.each do |code, qualifier|
-      code.is_a?(Fixnum).should be_true
+      code.is_a?(String).should be_true
       qualifier.is_a?(String).should be_true
     end
   end
   
   it "should retrieve a rejection qualifier for a given code" do
-    reason = Absa::H2h::Eft::RejectionCode.qualifier_for_code(2)
+    reason = Absa::H2h::Eft::RejectionCode.qualifier_for_code("2")
     reason.should == "COURT ORDER HOLD ON ACCOUNT"
   end
   
